64c0c79b0b6a2f4978b04b23aa1b534da16bb68c
Paul Hawxby <phawxby@gmail.com>
2012-07-27 15:07:54 +0100

    First commit
    Credit: Phil Haack
    Article:
    http://haacked.com/archive/2011/07/14/model-metadata-and-validation-localization-using-conventions.aspx

---
+++ ModelMetadataDemo.Web/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+using System.Web.Mvc;
+using System.Web.Routing;
+using ModelMetadataDemo.Web.Models;
+
+namespace ModelMetadataDemo.Web.Controllers {
+    public class HomeController : Controller {
+        protected override void Initialize(RequestContext requestContext) {
+            var culture = requestContext.HttpContext.Request.QueryString["culture"];
+            if (!String.IsNullOrEmpty(culture)) {
+                SetCulture(culture);
+            }
+            base.Initialize(requestContext);
+        }
+
+        public ActionResult Index() {
+            ViewBag.Message = "Welcome to ASP.NET MVC!";
+            return View(new Character());
+        }
+
+        private void SetCulture(string culture) {
+            culture = culture ?? "en";
+            ViewBag.Culture = culture;
+            Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(culture);
+            Thread.CurrentThread.CurrentUICulture = CultureInfo.CreateSpecificCulture(culture);
+        }
+
+        [HttpPost]
+        public ActionResult Index(Character character) {
+            ViewBag.Message = "Welcome to ASP.NET MVC!";
+            return View(character);
+        }
+
+        public ActionResult About() {
+            return View();
+        }
+    }
+}
---
+++ ModelMetadataDemo.Web/Models/Character.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModelMetadataDemo.Web.Models {
+    public class Character {
+        [Required]
+        public string FirstName { get; set; }
+
+        [Display(Name = "LastNameLabel")]
+        [Required]
+        public string LastName { get; set; }
+
+        public int HitPoints { get; set; }
+
+        [Display(Name = "The Level (from DisplayAttribute)")]
+        public int Level { get; set; }
+
+        public int ArmorClass { get; set; }
+    }
+}
